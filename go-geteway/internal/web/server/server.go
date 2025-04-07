25cb7adbc5399d404ab9234fa9876ebb1cde40cc
Gustavo Oliveira <guoliveeira@icloud.com>
2025-04-07 13:18:33 -0300

    :tada: Commit inicial

---
+++ go-geteway/internal/web/server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/oliveiraagu1/payment/go-geteway/internal/service"
+	"github.com/oliveiraagu1/payment/go-geteway/internal/web/handlers"
+	"net/http"
+)
+
+type Server struct {
+	router         *chi.Mux
+	server         *http.Server
+	accountService *service.AccountService
+	port           string
+}
+
+func NewServer(accountService *service.AccountService, port string) *Server {
+	return &Server{
+		router:         chi.NewRouter(),
+		accountService: accountService,
+		port:           port,
+	}
+}
+
+func (s *Server) ConfigureRoutes() {
+	accountHandler := handlers.NewAccountHandler(s.accountService)
+
+	s.router.Post("/accounts", accountHandler.Create)
+	s.router.Get("/accounts", accountHandler.Get)
+}
+
+func (s *Server) Start() error {
+	s.server = &http.Server{
+		Addr:    ":" + s.port,
+		Handler: s.router,
+	}
+	return s.server.ListenAndServe()
+}
