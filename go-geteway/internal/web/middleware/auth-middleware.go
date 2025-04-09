0c09e2b03d99429c9ddeea5ef888ffc922043219
Gustavo Oliveira <guoliveeira@icloud.com>
2025-04-09 18:23:24 -0300

    :sparkles: feat: Incluindo o invoice

---
+++ go-geteway/internal/web/middleware/auth-middleware.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"github.com/oliveiraagu1/payment/go-geteway/internal/domain"
+	"github.com/oliveiraagu1/payment/go-geteway/internal/service"
+	"net/http"
+)
+
+type AuthMiddleware struct {
+	accountService *service.AccountService
+}
+
+func NewAuthMiddleware(accountService *service.AccountService) *AuthMiddleware {
+	return &AuthMiddleware{
+		accountService: accountService,
+	}
+}
+
+func (m *AuthMiddleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-KEY")
+		if apiKey == "" {
+			http.Error(w, "X-API_KEY is required", http.StatusUnauthorized)
+			return
+		}
+
+		_, err := m.accountService.FindByAPIKey(apiKey)
+		if err != nil {
+			if err == domain.ErrAccountNotFound {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- go-geteway/internal/web/server/server.go
+++ go-geteway/internal/web/server/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/oliveiraagu1/payment/go-geteway/internal/service"
 	"github.com/oliveiraagu1/payment/go-geteway/internal/web/handlers"
+	"github.com/oliveiraagu1/payment/go-geteway/internal/web/middleware"
 	"net/http"
 )
 
@@ -11,22 +12,33 @@ type Server struct {
 	router         *chi.Mux
 	server         *http.Server
 	accountService *service.AccountService
+	invoiceService *service.InvoiceService
 	port           string
 }
 
-func NewServer(accountService *service.AccountService, port string) *Server {
+func NewServer(accountService *service.AccountService, invoiceService *service.InvoiceService, port string) *Server {
 	return &Server{
 		router:         chi.NewRouter(),
 		accountService: accountService,
+		invoiceService: invoiceService,
 		port:           port,
 	}
 }
 
 func (s *Server) ConfigureRoutes() {
 	accountHandler := handlers.NewAccountHandler(s.accountService)
+	invoiceHandler := handlers.NewInvoiceHandler(s.invoiceService)
+	authMiddleware := middleware.NewAuthMiddleware(s.accountService)
 
 	s.router.Post("/accounts", accountHandler.Create)
 	s.router.Get("/accounts", accountHandler.Get)
+
+	s.router.Group(func(r chi.Router) {
+		r.Use(authMiddleware.Authenticate)
+		s.router.Post("/invoice", invoiceHandler.Create)
+		s.router.Get("/invoice/{id}", invoiceHandler.GetByID)
+		s.router.Get("/invoice", invoiceHandler.ListByAccount)
+	})
 }
 
 func (s *Server) Start() error {
